d5cb0b5389a733ca90c0d4ca9330bc377171cec2
Phil Carney <philipccarney@gmail.com>
2019-05-26 14:48:02 +0100

    Amend source paths, add disclaimer.

--- install.sh
+++ install.sh
@@ -1,18 +1,26 @@
 #!/usr/bin/env bash
 
+# Treat this script as a work in progress.
+#
+#
+# This script assumes that the repository is cloned to $HOME/dotfiles
+# Adjust the path as required if this isn't right for you.
+
 echo "pwsh profile"
-ln -sf "./pwsh/profile.ps1" "$HOME/.config/powershell/profile.ps1"
+ln -sf "$HOME/dotfiles/pwsh/profile.ps1" "$HOME/.config/powershell/profile.ps1"
 echo "PSReadline history"
 ln -sf "$HOME/.local/share/powershell/PSReadLine/ConsoleHost_history.txt" "$HOME/.ps_history.txt"
 echo "tmux config"
-ln -sf "./tmux/.tmux.conf" "$HOME/.tmux.conf"
+ln -sf "$HOME/dotfiles/tmux/.tmux.conf" "$HOME/.tmux.conf"
 echo "tmux default session"
-ln -sf "./tmux/.tmux.conf" "$HOME/.tmux-default"
+ln -sf "$HOME/dotfiles/tmux/.tmux.conf" "$HOME/.tmux-default"
 echo "Visual Studio Code settings"
-ln -sf "./vscode/settings.json" "$HOME/.config/Code/User/settings.json"
+ln -sf "$HOME/dotfiles/vscode/settings.json" "$HOME/.config/Code/User/settings.json"
 echo "Visual Studio Code keybindings"
-ln -sf "./vscode/keybindings.json" "$HOME/.config/Code/User/keybindings.json"
+ln -sf "$HOME/dotfiles/vscode/keybindings.json" "$HOME/.config/Code/User/keybindings.json"
+echo "Visual Studio Code Ansible snippets"
+ln -sf "$HOME/dotfiles/vscode/ansible.json" "$HOME/.config/Code/User/snippets/ansible.json"
 echo "Visual Studio Code Powershell snippets"
-ln -sf "./vscode/powershell.json" "$HOME/.config/Code/User/snippets/powershell.json"
+ln -sf "$HOME/dotfiles/vscode/powershell.json" "$HOME/.config/Code/User/snippets/powershell.json"
 echo "Vim config"
-ln -sf "./vim/vimrc" "$HOME/.vimrc"
+ln -sf "$HOME/dotfiles/vim/vimrc" "$HOME/.vimrc"
