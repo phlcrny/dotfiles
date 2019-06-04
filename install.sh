490be193360956808c2eb7246d1ac306cc34bd4e
Phil Carney <philipccarney@gmail.com>
2019-06-04 18:55:34 +0100

    Amend $HOME for ~

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # Treat this script as (at best) a work in progress.
 #
-# This script assumes that the repository is cloned to $HOME/dotfiles
+# This script assumes that the repository is cloned to ~/dotfiles
 # Adjust the path as required if this isn't right for you.
 
 # bash
@@ -17,32 +17,32 @@ ln -sf ~dotfiles/bash/bash_aliases ~/.bash_aliases
 
 # tmux
 echo "tmux config"
-ln -sf "$HOME/dotfiles/tmux/.tmux.conf" "$HOME/.tmux.conf"
+ln -sf "~/dotfiles/tmux/.tmux.conf" "~/.tmux.conf"
 echo "tmux default session"
-ln -sf "$HOME/dotfiles/tmux/.tmux.conf" "$HOME/.tmux-default"
+ln -sf "~/dotfiles/tmux/.tmux.conf" "~/.tmux-default"
 
 # vim
 echo "vim config"
-ln -sf "$HOME/dotfiles/vim/vimrc" "$HOME/.vimrc"
+ln -sf "~/dotfiles/vim/vimrc" "~/.vimrc"
 
 # pwsh
 if [-x "$(command -v pwsh)"]; then
-    mkdir -p "$HOME/.config/powershell"
+    mkdir -p "~/.config/powershell"
     echo "pwsh profile"
-    ln -sf "$HOME/dotfiles/pwsh/profile.ps1" "$HOME/.config/powershell/profile.ps1"
+    ln -sf "~/dotfiles/pwsh/profile.ps1" "~/.config/powershell/profile.ps1"
     echo "PSReadline history"
-    ln -sf "$HOME/.local/share/powershell/PSReadLine/ConsoleHost_history.txt" "$HOME/.ps_history.txt"
+    ln -sf "~/.local/share/powershell/PSReadLine/ConsoleHost_history.txt" "~/.ps_history.txt"
 fi
 
 # vscode
 if [-x "$(command -v code)"]; then
-    mkdir -p "$HOME/.config/vscode"
+    mkdir -p "~/.config/vscode"
     echo "Visual Studio Code settings"
-    ln -sf "$HOME/dotfiles/vscode/settings.json" "$HOME/.config/Code/User/settings.json"
+    ln -sf "~/dotfiles/vscode/settings.json" "~/.config/Code/User/settings.json"
     echo "Visual Studio Code keybindings"
-    ln -sf "$HOME/dotfiles/vscode/keybindings.json" "$HOME/.config/Code/User/keybindings.json"
+    ln -sf "~/dotfiles/vscode/keybindings.json" "~/.config/Code/User/keybindings.json"
     echo "Visual Studio Code Ansible snippets"
-    ln -sf "$HOME/dotfiles/vscode/ansible.json" "$HOME/.config/Code/User/snippets/ansible.json"
+    ln -sf "~/dotfiles/vscode/ansible.json" "~/.config/Code/User/snippets/ansible.json"
     echo "Visual Studio Code Powershell snippets"
-    ln -sf "$HOME/dotfiles/vscode/powershell.json" "$HOME/.config/Code/User/snippets/powershell.json"
+    ln -sf "~/dotfiles/vscode/powershell.json" "~/.config/Code/User/snippets/powershell.json"
 fi
