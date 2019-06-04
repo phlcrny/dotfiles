2eb08baaae23367c086e46b61aede0568f9ea4b2
Phil Carney <philipccarney@gmail.com>
2019-06-04 19:09:38 +0100

    Add missing directories.

--- install.sh
+++ install.sh
@@ -31,17 +31,19 @@ if [ -x "$(command -v pwsh)" ]; then
     echo "pwsh profile"
     ln -sf "~/dotfiles/pwsh/profile.ps1" "~/.config/powershell/profile.ps1"
     echo "PSReadline history"
+    mkdir -p "~/.local/share/powershell/PSReadLine/"
     ln -sf "~/.local/share/powershell/PSReadLine/ConsoleHost_history.txt" "~/.ps_history.txt"
 fi
 
 # vscode
 if [ -x "$(command -v code)" ]; then
-    mkdir -p "~/.config/vscode"
+    mkdir -p "~/.config/Code/User"
     echo "Visual Studio Code settings"
     ln -sf "~/dotfiles/vscode/settings.json" "~/.config/Code/User/settings.json"
     echo "Visual Studio Code keybindings"
     ln -sf "~/dotfiles/vscode/keybindings.json" "~/.config/Code/User/keybindings.json"
     echo "Visual Studio Code Ansible snippets"
+    mkdir -p "~/.config/Code/snippets"
     ln -sf "~/dotfiles/vscode/ansible.json" "~/.config/Code/User/snippets/ansible.json"
     echo "Visual Studio Code Powershell snippets"
     ln -sf "~/dotfiles/vscode/powershell.json" "~/.config/Code/User/snippets/powershell.json"
