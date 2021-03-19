8d473e6c12658f1cbd63137f05589f5719847e9e
Phil Carney <philipccarney@gmail.com>
2021-03-19 19:39:35 +0000

    Update messages

--- install.sh
+++ install.sh
@@ -6,39 +6,39 @@
 # Adjust the path as required if this isn't right for you.
 
 # bash
-echo "Processing bash profile"
+echo "Installing bash profile symlink"
 ln -sf ~/dotfiles/bash/bash_profile ~/.bash_profile
-echo "Processing Generic bash profile"
+echo "Installing Generic bash profile symlink"
 ln -sf ~/dotfiles/bash/bash_profile ~/.profile
-echo "Processing bashrc"
+echo "Installing bashrc symlink"
 ln -sf ~/dotfiles/bash/bashrc ~/.bashrc
-echo "Processing bash aliases"
+echo "Installing bash aliases symlink"
 ln -sf ~/dotfiles/bash/bash_aliases ~/.bash_aliases
 # tmux
-echo "Processing tmux config"
+echo "Installing tmux config symlink"
 ln -sf ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
 # vim
-echo "Processing vim config"
+echo "Installing vim config symlink"
 ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
 # pwsh
 if [ -x "$(command -v pwsh)" ]; then
     mkdir -p ~/.config/powershell
-    echo "Processing pwsh profile"
+    echo "Installing pwsh profile symlink"
     ln -sf ~/dotfiles/pwsh/profile.ps1 ~/.config/powershell/profile.ps1
-    echo "Processing PSReadline history"
+    echo "Installing PSReadline history symlink"
     mkdir -p ~/.local/share/powershell/PSReadLine/
     ln -sf ~/.local/share/powershell/PSReadLine/ConsoleHost_history.txt ~/.ps_history.txt
 fi
 # vscode
 if [ -x "$(command -v code)" ]; then
     mkdir -p ~/.config/Code/User
-    echo "Processing Visual Studio Code settings"
+    echo "Installing Visual Studio Code settings symlink"
     ln -sf ~/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
-    echo "Processing Visual Studio Code keybindings"
+    echo "Installing Visual Studio Code keybindings symlink"
     ln -sf ~/dotfiles/vscode/keybindings.json ~/.config/Code/User/keybindings.json
-    echo "Processing Visual Studio Code Ansible snippets"
+    echo "Installing Visual Studio Code Ansible snippets symlink"
     mkdir -p ~/.config/Code/snippets
     ln -sf ~/dotfiles/vscode/ansible.json ~/.config/Code/User/snippets/ansible.json
-    echo "Processing Visual Studio Code Powershell snippets"
+    echo "Installing Visual Studio Code Powershell snippets symlink"
     ln -sf ~/dotfiles/vscode/powershell.json ~/.config/Code/User/snippets/powershell.json
 fi
